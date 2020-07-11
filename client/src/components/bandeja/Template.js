1f09b27b776f24e03e33a5446438c14824e23b40
k0o <sb.knot@gmail.com>
2020-07-10 23:40:43 -0400

    fix: answerInput wrong size regard to parent

--- client/src/components/bandeja/Template.js
+++ client/src/components/bandeja/Template.js
@@ -111,6 +111,7 @@ export default function Template() {
                                         value={answer}
                                         contentEditable
                                         ref={ansInput}
+                                        spellCheck="false"
                                     ></div>
                                 </div>
                             </div>
