8e2efcf4daa594d736cedf5c3da756738f269461
k0o <sb.knot@gmail.com>
2020-08-16 21:55:04 -0400

    chore: adjust style loader

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,3 +1,16 @@
-const { useBabelRc, override, useEslintRc } = require('customize-cra')
+const { useBabelRc, override, useEslintRc, adjustStyleLoaders } = require('customize-cra')
 
-module.exports = override(useBabelRc(), useEslintRc())
+module.exports = override(
+    useBabelRc(),
+    useEslintRc(),
+    adjustStyleLoaders((({ use: [, css] }) => {
+        css.options = {
+            modules: {
+                localIdentName: (process.env.NODE_ENV === 'development'
+                    ? '[name]__[local]__[hash:base64:5]'
+                    : '[hash:base64:6]'
+                )
+            },
+        }
+    }))
+)
