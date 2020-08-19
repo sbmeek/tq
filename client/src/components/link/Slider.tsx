ee1ce348a762dbd9009c3efc5728cc16cc9522b8
k0o <sb.knot@gmail.com>
2020-08-19 00:05:32 -0400

    feat (link): blurred placeholder

--- client/src/components/link/Slider.tsx
+++ client/src/components/link/Slider.tsx
@@ -1,42 +1,61 @@
-import React, { useState } from 'react'
-import ft1 from '../../assets/images/link/ft1.png'
-import ft2 from '../../assets/images/link/ft2.png'
-import ft3 from '../../assets/images/link/ft3.png'
-import ft4 from '../../assets/images/link/ft4.png'
-import ft5 from '../../assets/images/link/ft5.png'
-
+import React, { useState, TransitionEvent } from 'react'
+import __ft1Thumbnail from 'assets/images/link/__ft1Thumbnail.jpg'
+import ft1 from 'assets/images/link/ft1.png'
+import ft2 from 'assets/images/link/ft2.png'
+import ft3 from 'assets/images/link/ft3.png'
+import ft4 from 'assets/images/link/ft4.png'
+import ft5 from 'assets/images/link/ft5.png'
 import { Carousel } from 'react-responsive-carousel'
 import './Slider.css'
+
 // Number of images that have already loaded
-let n = 0
+let n = 0;
 
 function Slider() {
 	const [imagesLoaded, setImagesLoaded] = useState(false)
+    const [placeholderLoaded, setPlaceholderLoaded] = useState(false)
 
 	const handleImageLoad = () => {
-		n++
+        n++;
 		setImagesLoaded(n >= 7)
+	}
+
+	const preloaderTransitionEndHandler = (
+		e: TransitionEvent<HTMLImageElement>
+	) => {
+		const targetElement = e.target as HTMLImageElement
+		if (e.propertyName === 'opacity') {
+			targetElement.parentElement!.className = ''
+			targetElement.remove()
+		}
 	}
 
 	return (
 		<Carousel
 			infiniteLoop={true}
-			autoPlay={false}
+			autoPlay={imagesLoaded}
 			interval={2000}
 			showStatus={false}
 			showThumbs={false}
+			showArrows={imagesLoaded}
+			showIndicators={imagesLoaded}
 		>
+            <div styleName={`preloader ${placeholderLoaded ? 'mobilePlaceholder'  : ''} ${imagesLoaded && placeholderLoaded ? 'disabled' : ''}`}>
+                <img
+                    src={__ft1Thumbnail}
+                    styleName="img-placeholder"
+                    alt="placeholder"
+                    onTransitionEnd={preloaderTransitionEndHandler}
+                    onLoad={() => setPlaceholderLoaded(true)}
+                />
+                <img
+                    src={ft1}
+                    alt="ft1"
+                    styleName={`imgFt1 ${!imagesLoaded ? 'loading' : ''}`}
+                    onLoad={() => handleImageLoad()}
+                />
+            </div>
 			<div>
-				{!imagesLoaded && <div styleName="preloader"></div>}
-				<img
-					src={ft1}
-					alt="ft1"
-					styleName={!imagesLoaded ? 'loading' : ''}
-					onLoad={() => handleImageLoad()}
-				/>
-			</div>
-			<div>
-				{!imagesLoaded && <div styleName="preloader"></div>}
 				<img
 					src={ft2}
 					styleName={!imagesLoaded ? 'loading' : ''}
@@ -45,7 +64,6 @@ function Slider() {
 				/>
 			</div>
 			<div>
-				{!imagesLoaded && <div styleName="preloader"></div>}
 				<img
 					src={ft3}
 					styleName={!imagesLoaded ? 'loading' : ''}
@@ -54,7 +72,6 @@ function Slider() {
 				/>
 			</div>
 			<div>
-				{!imagesLoaded && <div styleName="preloader"></div>}
 				<img
 					src={ft4}
 					styleName={!imagesLoaded ? 'loading' : ''}
@@ -63,7 +80,6 @@ function Slider() {
 				/>
 			</div>
 			<div>
-				{!imagesLoaded && <div styleName="preloader"></div>}
 				<img
 					src={ft5}
 					styleName={!imagesLoaded ? 'loading' : ''}
