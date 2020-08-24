c1f5ca1789489db190e7461110568b462e4b99b4
k0o <sb.knot@gmail.com>
2020-08-23 22:08:49 -0400

    refactor (template): editor

--- client/src/components/inb/template/Template.tsx
+++ client/src/components/inb/template/Template.tsx
@@ -1,48 +1,32 @@
 import React, {
 	useState,
-	useContext,
 	useEffect,
 	useRef,
-	FormEvent,
-	KeyboardEvent,
 	MouseEvent,
+	FormEvent,
+	useContext,
 } from 'react'
 import { useLocation, Link } from 'react-router-dom'
 import parse from 'html-react-parser'
-import { InitContext } from 'global/context/InitContext'
 import styles from './Template.css'
-import BgColors from './templateOpts/BgColors'
-import Labels from './templateOpts/Labels'
 import ReplyingModal from '../replyingModal/ReplyingModal'
-
-interface IOptsVisibility {
-	bgColorsOptShown: boolean
-	labelsOptShown: boolean
-	[key: string]: boolean
-}
+import TemplateEditor from './templateEditor/TemplateEditor'
+import { InitContext } from 'global/context/InitContext'
 
 export default function Template() {
 	const [answer, setAnswer] = useState<string>('')
 	const [showLabel, setShowLabel] = useState<boolean>(false)
 	const [actualMsg, setActualMsg] = useState<ITQMessage>()
-    const [isReplyingModalOpened, setIsReplyingModalOpened] = useState(false)
-	const InitialStateOptsVisibility: IOptsVisibility = {
-		bgColorsOptShown: false,
-		labelsOptShown: false,
-	}
-	const [optsVisibility, setOptsVisibility] = useState<IOptsVisibility>(
-		InitialStateOptsVisibility
-	)
-	const { state: { socket } } = useContext(InitContext)
-	const [isPlaceholderVisible, setIsPlaceholderVisible] = useState(true)
+	const [isReplyingModalOpened, setIsReplyingModalOpened] = useState(false)
 	const templateQuestion = useRef<HTMLDivElement>(null)
 	const templateAnswer = useRef<HTMLDivElement>(null)
 	const templateQuestionContainer = useRef<HTMLDivElement>(null)
-	const ansInput = useRef<HTMLDivElement>(null)
-	const optsSwipe = useRef<HTMLDivElement>(null)
 	const label = useRef<HTMLDivElement>(null)
 	const form = useRef<HTMLFormElement>(null)
 	const location = useLocation<ITQMessage>()
+	const {
+		state: { socket },
+	} = useContext(InitContext)
 
 	useEffect(() => {
 		const msg = (location.state as any)['actualMsg'] as ITQMessage
@@ -62,66 +46,6 @@ export default function Template() {
 			// 	//success
 			// }
 		})
-	}
-
-	const handleInput = async (e: KeyboardEvent<HTMLDivElement>) => {
-		const targetElement = e.currentTarget as HTMLDivElement
-		const { textContent: ansVal } = targetElement
-		setIsPlaceholderVisible(ansVal!.length < 1)
-		await setAnswer(targetElement.innerHTML)
-	}
-
-	const handleBtnFormatClick = (e: MouseEvent<HTMLButtonElement>) => {
-		const targetElement = e.target as HTMLButtonElement
-		const { parentElement: target } = targetElement
-		formatAnswer(target!.title.toLowerCase())
-	}
-
-	const formatAnswer = (cmd: string) => {
-		document.execCommand(cmd, false, '')
-		setAnswer(ansInput.current!.innerHTML)
-		ansInput.current!.focus()
-	}
-
-	const toggleOptSwipe = (e: MouseEvent<HTMLButtonElement>) => {
-		const targetElement = e.target as HTMLButtonElement
-		const optSwp = optsSwipe.current!
-		let _dsrdOpt = getDesiredOpt(targetElement.id)
-		let desiredOpt = Object.keys(_dsrdOpt)[0]
-		let _optsVisibility = {
-			...InitialStateOptsVisibility,
-			[Object.keys(_dsrdOpt)[0]]: true,
-        }
-        
-		if (targetElement.id === 'text')
-			_optsVisibility = InitialStateOptsVisibility
-
-		if (optSwp!.classList.contains('opts-swipe_actv')) {
-			if (optsVisibility[desiredOpt]) {
-				optSwp.classList.remove('opts-swipe_actv')
-				_optsVisibility = InitialStateOptsVisibility
-			} else {
-				_optsVisibility = {
-					...InitialStateOptsVisibility,
-					[Object.keys(_dsrdOpt)[0]]: true,
-				}
-			}
-		} else {
-			optSwp.classList.toggle('opts-swipe_actv')
-		}
-		if (targetElement.id === 'text') optSwp.classList.remove('opts-swipe_actv')
-		setOptsVisibility(_optsVisibility)
-	}
-
-	const getDesiredOpt = (_id: string) => {
-		switch (_id) {
-			case 'bg-colors':
-				return { bgColorsOptShown: true }
-			case 'labels':
-				return { labelsOptShown: true }
-			default:
-				return {}
-		}
 	}
 
 	const toggleLabelHandler = (e: MouseEvent<HTMLDivElement>) => {
@@ -165,152 +89,21 @@ export default function Template() {
 					opened={isReplyingModalOpened}
 					setOpened={setIsReplyingModalOpened}
 					form={form.current as HTMLFormElement}
-                    templateQuestion={templateQuestionContainer.current as HTMLDivElement}
+					templateQuestion={templateQuestionContainer.current as HTMLDivElement}
 				/>
-				<div styleName="template-answer">
-					<form
-						styleName="template-answer-form-container"
-						onSubmit={handleFormSubmit}
-						ref={form}
-					>
-						<div>
-							<div styleName="template-answer-btns-format-container">
-								<div styleName="template-answer-btns-format">
-									<button
-										title="Bold"
-										type="button"
-										onClick={handleBtnFormatClick}
-									>
-										<i className="material-icons">format_bold</i>
-									</button>
-									<button
-										title="Italic"
-										type="button"
-										onClick={handleBtnFormatClick}
-									>
-										<i className="material-icons">format_italic</i>
-									</button>
-									<button
-										title="Underline"
-										type="button"
-										onClick={handleBtnFormatClick}
-									>
-										<i className="material-icons">format_underlined</i>
-									</button>
-								</div>
-								<button type="button">
-									<i className="material-icons">emoji_emotions</i>
-								</button>
-							</div>
-							<div styleName="template-input-answer-container">
-								<div styleName="template-input-answer-inner-container">
-									<div>
-										<div
-											style={{
-												visibility: isPlaceholderVisible ? 'visible' : 'hidden',
-											}}
-											styleName="template-input-answer-placeholder"
-										>
-											Escribe tu respuesta...
-										</div>
-										<div
-											styleName="template-input-answer"
-											data-name="ans-msg"
-											onInput={handleInput}
-											data-value={answer}
-											contentEditable
-											ref={ansInput}
-											spellCheck="false"
-										></div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div styleName="template-answer-options-container">
-							<div styleName="template-answer-options">
-								<button type="button" onClick={toggleOptSwipe} id="text">
-									<svg
-										width="100%"
-										height="100%"
-										viewBox="0 0 1080 1080"
-										version="1.1"
-										xmlns="http://www.w3.org/2000/svg"
-										xmlnsXlink="http://www.w3.org/1999/xlink"
-										xmlSpace="preserve"
-										style={{
-											fillRule: 'evenodd',
-											clipRule: 'evenodd',
-											strokeLinecap: 'round',
-											strokeLinejoin: 'round',
-											strokeMiterlimit: 1.5,
-										}}
-									>
-										<rect
-											id="Artboard1"
-											x="0"
-											y="0"
-											width="1080"
-											height="1080"
-											style={{ fill: 'none' }}
-										/>
-										<g id="Artboard11">
-											<path
-												d="M270.271,1040.47c-8.111,0 -15.89,-3.222 -21.626,-8.957c-5.735,-5.736 -8.957,-13.515 -8.957,-21.626c-0,-0.02 -0,-0.04 -0,-0.06c0.005,-16.218 11.965,-29.95 28.028,-32.183c18.421,-1.443 31.953,-5.344 40.656,-11.287c12.665,-8.648 18.998,-23.543 18.998,-44.684c-0,0 -0,-354.375 -0,-589.717c-0,-54.256 -21.553,-106.289 -59.918,-144.654c-38.364,-38.364 -90.397,-59.917 -144.653,-59.917c-0.013,0 -0.02,0 -0.02,0c-17.329,0 -46.312,15.014 -72.847,34.609c-9.663,6.768 -22.378,7.302 -32.575,1.37c-10.197,-5.933 -16.016,-17.251 -14.908,-28.996c-0.032,-1.144 -0.049,-0.856 0,-1.372c4.976,-52.748 46.827,-93.474 99.81,-93.474c152.115,-0 451.338,-0 601.807,-0c51.583,-0 94.733,39.174 99.703,90.517c0.043,0.448 0.087,0.897 0.13,1.345c1.235,12.847 -5.069,25.25 -16.174,31.827c-11.105,6.576 -25.011,6.14 -35.683,-1.119c-25.66,-20.715 -55.565,-34.707 -73.945,-34.707c0,0 -0.007,0 -0.02,0c-54.256,0 -106.289,21.553 -144.653,59.917c-38.365,38.365 -59.918,90.398 -59.918,144.654c0,235.342 0,589.717 0,589.717c0,21.141 6.333,36.036 18.998,44.684c8.703,5.943 22.235,9.844 40.598,11.703c15.854,2.204 27.655,15.76 27.655,31.767c0.431,0.02 0.431,0.04 0.431,0.06c0,8.111 -3.222,15.89 -8.958,21.626c-5.735,5.735 -13.514,8.957 -21.625,8.957c-61.383,0 -198.951,0 -260.334,0Z"
-												style={{ fill: '#fff', fillRule: 'nonzero' }}
-											/>
-											<path
-												d="M1054,39.526l0,1000.95"
-												style={{
-													fill: 'none',
-													stroke: '#fff',
-													strokeWidth: '43.63px',
-												}}
-											/>
-										</g>
-									</svg>
-								</button>
-								<button type="button" onClick={toggleOptSwipe} id="bg-colors">
-									S
-								</button>
-								<button type="button" onClick={toggleOptSwipe} id="labels">
-									P
-								</button>
-								<button type="button" onClick={toggleOptSwipe}>
-									M
-								</button>
-								<button type="button" onClick={toggleOptSwipe}>
-									R
-								</button>
-							</div>
-						</div>
-						<div
-							ref={optsSwipe}
-							styleName="template-answer-opts-menu"
-							className="opts-swipe_inactv"
-						>
-							<div
-								style={{
-									height: optsVisibility.bgColorsOptShown ? '100%' : '0',
-								}}
-							>
-								<BgColors
-									templateQuestionContainer={templateQuestionContainer.current!}
-								/>
-							</div>
-							<div
-								style={{
-									height: optsVisibility.labelsOptShown ? '100%' : '0',
-								}}
-							>
-								<Labels label={label.current!} setShowLabel={setShowLabel} />
-							</div>
-						</div>
-					</form>
-				</div>
+				<TemplateEditor
+                    form={form}
+                    label={label}
+					answer={answer}
+                    setAnswer={setAnswer}
+                    setShowLabel={setShowLabel}
+                    handleFormSubmit={handleFormSubmit}
+                    templateQuestionContainer={templateQuestionContainer!.current}
+				/>
 			</div>
 			<div
 				styleName="template-question-container"
-                ref={templateQuestionContainer}
+				ref={templateQuestionContainer}
 				className="d-text-select"
 			>
 				<div
---
+++ client/src/components/inb/template/templateEditor/TemplateEditor.tsx
@@ -0,0 +1,216 @@
+import React, {
+	useState,
+	MouseEvent,
+	KeyboardEvent,
+	useRef,
+	FormEvent,
+} from 'react'
+import '../Template.css'
+import './TemplateEditor.css'
+import BgColors from '../templateOpts/BgColors'
+import Labels from '../templateOpts/Labels'
+import textIcon from 'assets/images/templateEditor-icons/text-icon.svg';
+
+interface IProps {
+	answer: string
+    form: React.RefObject<HTMLFormElement>
+    label: React.RefObject<HTMLDivElement>
+	setAnswer: React.Dispatch<React.SetStateAction<string>>
+    setShowLabel: React.Dispatch<React.SetStateAction<boolean>>
+    handleFormSubmit: (e: FormEvent<HTMLFormElement>) => void
+    templateQuestionContainer: HTMLDivElement | null
+}
+
+export default function TemplateEditor({
+    form,
+    label,
+	answer,
+	setAnswer,
+    setShowLabel,
+    handleFormSubmit,
+    templateQuestionContainer,
+}: IProps) {
+	const [isPlaceholderVisible, setIsPlaceholderVisible] = useState(true)
+	const InitialStateOptsVisibility: { [key: string]: boolean } = {
+		bgColorsOptShown: false,
+		labelsOptShown: false,
+	}
+	const [optsVisibility, setOptsVisibility] = useState(
+		InitialStateOptsVisibility
+	)
+	const ansInput = useRef<HTMLDivElement>(null)
+	const optsSwipe = useRef<HTMLDivElement>(null)
+
+	const getDesiredOpt = (_id: string) => {
+		switch (_id) {
+			case 'bg-colors':
+				return { bgColorsOptShown: true }
+			case 'labels':
+				return { labelsOptShown: true }
+			default:
+				return {}
+		}
+	}
+
+	const toggleOptSwipe = (e: MouseEvent<HTMLButtonElement>) => {
+		const targetElement = e.target as HTMLButtonElement
+		const optSwp = optsSwipe.current!
+		let _dsrdOpt = getDesiredOpt(targetElement.id)
+		let desiredOpt = Object.keys(_dsrdOpt)[0]
+		let _optsVisibility = {
+			...InitialStateOptsVisibility,
+			[Object.keys(_dsrdOpt)[0]]: true,
+		}
+
+		if (targetElement.id === 'text')
+			_optsVisibility = InitialStateOptsVisibility
+
+		if (optSwp!.classList.contains('opts-swipe_actv')) {
+			if (optsVisibility[desiredOpt]) {
+				optSwp.classList.remove('opts-swipe_actv')
+				_optsVisibility = InitialStateOptsVisibility
+			} else {
+				_optsVisibility = {
+					...InitialStateOptsVisibility,
+					[Object.keys(_dsrdOpt)[0]]: true,
+				}
+			}
+		} else {
+			optSwp.classList.toggle('opts-swipe_actv')
+		}
+		if (targetElement.id === 'text') optSwp.classList.remove('opts-swipe_actv')
+		setOptsVisibility(_optsVisibility)
+	}
+
+	const handleInput = (e: KeyboardEvent<HTMLDivElement>) => {
+		const targetElement = e.currentTarget as HTMLDivElement
+		const { textContent: ansVal } = targetElement
+		setIsPlaceholderVisible(ansVal!.length < 1)
+		setAnswer(targetElement.innerHTML)
+	}
+
+	const handleBtnFormatClick = (e: MouseEvent<HTMLButtonElement>) => {
+		const targetElement = e.target as HTMLButtonElement
+		const sel = window.getSelection()
+		console.log(
+			sel!.getRangeAt(0).commonAncestorContainer.parentElement,
+			sel?.toString()
+		)
+		const { parentElement: target } = targetElement
+		formatAnswer(target!.title.toLowerCase())
+	}
+
+	const formatAnswer = (cmd: string) => {
+		document.execCommand(cmd, false, '')
+		setAnswer(ansInput.current!.innerHTML)
+		ansInput.current!.focus()
+	}
+
+	return (
+		<div styleName="template-answer">
+			<form
+				styleName="template-answer-form-container"
+				onSubmit={handleFormSubmit}
+				ref={form}
+			>
+				<div>
+					<div styleName="template-answer-btns-format-container">
+						<div styleName="template-answer-btns-format">
+							<button
+								title="Bold"
+								type="button"
+								onClick={handleBtnFormatClick}
+								data-cmd="b"
+							>
+								<i className="material-icons">format_bold</i>
+							</button>
+							<button
+								title="Italic"
+								type="button"
+								onClick={handleBtnFormatClick}
+								data-cmd="i"
+							>
+								<i className="material-icons">format_italic</i>
+							</button>
+							<button
+								title="Underline"
+								type="button"
+								onClick={handleBtnFormatClick}
+								data-cmd="u"
+							>
+								<i className="material-icons">format_underlined</i>
+							</button>
+						</div>
+						<button type="button">
+							<i className="material-icons">emoji_emotions</i>
+						</button>
+					</div>
+					<div styleName="template-input-answer-container">
+						<div styleName="template-input-answer-inner-container">
+							<div>
+								<div
+									style={{
+										visibility: isPlaceholderVisible ? 'visible' : 'hidden',
+									}}
+									styleName="template-input-answer-placeholder"
+								>
+									Escribe tu respuesta...
+								</div>
+								<div
+									styleName="template-input-answer template-input-answer-sb"
+									data-name="ans-msg"
+									onInput={handleInput}
+									data-value={answer}
+									contentEditable
+									ref={ansInput}
+									spellCheck="false"
+								></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div styleName="template-answer-options-container">
+					<div styleName="template-answer-options template-answer-options-sb">
+						<button type="button" onClick={toggleOptSwipe} id="text">
+                            <img src={textIcon} alt="text icon" />
+						</button>
+						<button type="button" onClick={toggleOptSwipe} id="bg-colors">
+							S
+						</button>
+						<button type="button" onClick={toggleOptSwipe} id="labels">
+							P
+						</button>
+						<button type="button" onClick={toggleOptSwipe}>
+							M
+						</button>
+						<button type="button" onClick={toggleOptSwipe}>
+							R
+						</button>
+					</div>
+				</div>
+				<div
+					ref={optsSwipe}
+					styleName="template-answer-opts-menu"
+					className="opts-swipe_inactv"
+				>
+					<div
+						style={{
+							height: optsVisibility.bgColorsOptShown ? '100%' : '0',
+						}}
+					>
+						<BgColors
+							templateQuestionContainer={templateQuestionContainer!}
+						/>
+					</div>
+					<div
+						style={{
+							height: optsVisibility.labelsOptShown ? '100%' : '0',
+						}}
+					>
+						<Labels label={label.current!} setShowLabel={setShowLabel} />
+					</div>
+				</div>
+			</form>
+		</div>
+	)
+}
