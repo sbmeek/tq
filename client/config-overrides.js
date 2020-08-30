a0d2aa499014c04cb9e38000bacc36750ae0a7d7
k0o <sb.knot@gmail.com>
2020-08-30 17:45:39 -0400

    chore: adjust libs styles loaders

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,16 +1,48 @@
-const { useBabelRc, override, useEslintRc, adjustStyleLoaders } = require('customize-cra')
+const { useBabelRc, override, useEslintRc, adjustStyleLoaders, addWebpackModuleRule } = require('customize-cra')
 
 module.exports = override(
     useBabelRc(),
     useEslintRc(),
-    adjustStyleLoaders((({ use: [, css] }) => {
+    addWebpackModuleRule({
+        test: /emoji-mart.css$/,
+        use: [
+            {
+                loader: require.resolve('style-loader'),
+                options: { 
+                    attributes: { type: 'text/css' }
+                }
+            },
+            {
+                loader: require.resolve('css-loader'),
+                options: {
+                    modules: {
+                        localIdentName: '[local]',
+                    },
+                    onlyLocals: true
+                }
+            },
+        ],
+    }),
+    adjustStyleLoaders((loader) => {
+        const { use: [, css] } = loader;
+
+        /* eslint eqeqeq: "off" */
+        loader.exclude = /node_modules/;
         css.options = {
             modules: {
                 localIdentName: (process.env.NODE_ENV === 'development'
                     ? '[name]__[local]__[hash:base64:5]'
                     : '[hash:base64:6]'
-                )
+                ),
             },
         }
-    }))
+        if(loader.test.toString() == /emoji-mart.css$/){
+            delete loader.exclude
+            css.options = {
+                modules: {
+                    localIdentName: '[local]',
+                },
+            }
+        }
+    })
 )
