f06bed0428151eb3e8e1435c92a7f202fb78959a
k0o <sb.knot@gmail.com>
2020-09-25 16:44:41 -0400

    feat: transition between views

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,44 @@
-import React, { useEffect, useContext } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { useSelector, RootStateOrAny } from 'react-redux'
-import './App.css'
+import React, { useEffect, useContext } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useSelector, RootStateOrAny } from 'react-redux';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import './App.css';
 
-import Main from 'components/main/Main-idx'
-import Error404 from 'components/error/404'
-import Error500 from 'components/error/500'
-import Inbx from 'components/inb/Inb-idx'
-import AuthRoute from 'HOCs/AuthRoute'
-import UnauthRoute from 'HOCs/UnauthRoute'
-import UserLink from 'components/link/Link-idx'
-import TemplateMSG from 'components/inb/template/Template'
-import Msg from 'components/sendMsg/Msg-idx'
-import { InitContext } from 'global/context/InitContext'
-import Menu from 'components/partials/menu/Menu'
-import Terms from 'components/terms/Terms'
-import VerifyAccount from 'components/verifyAccount/VerifyAccount'
+import Main from 'components/main/Main-idx';
+import Error404 from 'components/error/404';
+import Error500 from 'components/error/500';
+import Inbx from 'components/inb/Inb-idx';
+import AuthRoute from 'HOCs/AuthRoute';
+import UnauthRoute from 'HOCs/UnauthRoute';
+import UserLink from 'components/link/Link-idx';
+import TemplateMSG from 'components/inb/template/Template';
+import Msg from 'components/sendMsg/Msg-idx';
+import { InitContext } from 'global/context/InitContext';
+import Menu from 'components/partials/menu/Menu';
+import Terms from 'components/terms/Terms';
+import VerifyAccount from 'components/verifyAccount/VerifyAccount';
 
-import AuthTester from './AuthTester'
+import AuthTester from './AuthTester';
 
 const loader = document.querySelector('#_l');
 
-const hideLoader = () => loader?.classList.add('rem')
+const hideLoader = () => loader?.classList.add('rem');
 
 export default function App() {
 	const { isStatus500, isLoaded, user } = useSelector(
 		(store: RootStateOrAny) => store.auth
-	)
-	let { socket, isTester } = useContext(InitContext).state
-	const testing = process.env.NODE_ENV === "production"
+	);
+	let { socket, isTester } = useContext(InitContext).state;
+	const testing = process.env.NODE_ENV === 'production';
 
 	useEffect(() => {
 		if (user !== undefined)
-			socket.emit('tq:init-user', { username: user.username })
-	}, [user, socket])
+			socket.emit('tq:init-user', { username: user.username });
+	}, [user, socket]);
 
 	if (!isStatus500) {
 		if (isLoaded) {
-            hideLoader();
+			hideLoader();
 			return (
 				<>
 					{!testing ? (
@@ -48,9 +49,9 @@ export default function App() {
 						<RqqtComp />
 					)}
 				</>
-			)
-		} else return <div></div>
-	} else return <Error500 />
+			);
+		} else return <div></div>;
+	} else return <Error500 />;
 }
 
 const RqqtComp = () => {
@@ -59,39 +60,59 @@ const RqqtComp = () => {
 			<Route path="/">
 				<Menu />
 			</Route>
-			<Switch>
-				<UnauthRoute
-					needsRenderTime={window.navigator.onLine}
-					exact
-					path="/"
-					component={Main}
-					redirectTo="/link"
-				/>
-				<AuthRoute
-					path="/messages"
-					component={Inbx}
-					redirectTo="/"
-					needsRenderTime={false}
-				/>
-				<AuthRoute
-					needsRenderTime={true}
-					path="/link"
-					component={UserLink}
-					redirectTo="/"
-				/>
-				<AuthRoute
-					needsRenderTime={false}
-					path="/message"
-					component={TemplateMSG}
-					redirectTo="/"
-				/>
-                <Route path="/account/verify" component={VerifyAccount} />
-				<Route path="/terms" component={Terms} />
-				<Route exact path="/:username" component={Msg} />
-				<Route path="*">
-					<Error404 />
-				</Route>
-			</Switch>
+			<Route
+				render={({ location }) => (
+					<TransitionGroup component={null}>
+						<CSSTransition
+							key={location.key}
+							appear={true}
+                            timeout={{ enter: 250, exit: 0 }}
+                            classNames="fade"
+						>
+							<Switch location={location}>
+								<UnauthRoute
+									needsRenderTime={window.navigator.onLine}
+									exact
+									path="/"
+									component={Main}
+									redirectTo="/link"
+								/>
+								<AuthRoute
+									path="/messages"
+									component={Inbx}
+									redirectTo="/"
+									needsRenderTime={false}
+								/>
+								<AuthRoute
+									needsRenderTime={false}
+									path="/link"
+									component={UserLink}
+									redirectTo="/"
+								/>
+								<AuthRoute
+									needsRenderTime={false}
+									path="/message"
+									component={TemplateMSG}
+									redirectTo="/"
+								/>
+								<Route
+									path="/account/verify"
+									component={VerifyAccount}
+								/>
+								<Route path="/terms" component={Terms} />
+								<Route
+									exact
+									path="/:username"
+									component={Msg}
+								/>
+								<Route path="*">
+									<Error404 />
+								</Route>
+							</Switch>
+						</CSSTransition>
+					</TransitionGroup>
+				)}
+			/>
 		</Router>
-	)
-}
+	);
+};
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,38 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './App'
-import generateStore from './global/store'
-import InitProvider from './global/context/InitContext'
-import * as serviceWorker from './serviceWorker'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import generateStore from './global/store';
+import InitProvider from './global/context/InitContext';
+import * as serviceWorker from './serviceWorker';
 
-const store = generateStore()
+const store = generateStore();
 
-const reactRoot = document.querySelector('#react-root')
+const reactRoot = document.querySelector('#react-root');
 function calcVH() {
 	var vH = Math.max(
 		document.documentElement.clientHeight,
 		window.innerHeight || 0
-	)
-	reactRoot!.setAttribute('style', 'height:' + vH + 'px;')
+	);
+	reactRoot!.setAttribute('style', 'height:' + vH + 'px;');
 }
 
-calcVH()
-window.addEventListener('onorientationchange', calcVH, true)
-window.addEventListener('resize', calcVH, true)
+calcVH();
+window.addEventListener('onorientationchange', calcVH, true);
+window.addEventListener('resize', calcVH, true);
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<InitProvider>
-				<App />
-			</InitProvider>
-		</Provider>
-	</React.StrictMode>,
+	<Provider store={store}>
+		<InitProvider>
+			<App />
+		</InitProvider>
+	</Provider>,
 	reactRoot
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
