06516362d6c2cb7ae12742a4032447a8f29d6b12
k0o <sb.knot@gmail.com>
2020-09-26 18:00:42 -0400

    perf: code splitting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useSelector, RootStateOrAny } from 'react-redux';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import './App.css';
-
-import Main from 'components/main/Main-idx';
-import Error404 from 'components/error/404';
-import Error500 from 'components/error/500';
-import Inbx from 'components/inb/Inb-idx';
-import AuthRoute from 'HOCs/AuthRoute';
-import UnauthRoute from 'HOCs/UnauthRoute';
-import UserLink from 'components/link/Link-idx';
-import TemplateMSG from 'components/inb/template/Template';
-import Msg from 'components/sendMsg/Msg-idx';
 import { InitContext } from 'global/context/InitContext';
-import Menu from 'components/partials/menu/Menu';
-import Terms from 'components/terms/Terms';
-import VerifyAccount from 'components/verifyAccount/VerifyAccount';
-
+import './App.css';
+import Loader from 'components/partials/loader/Loader';
 import AuthTester from './AuthTester';
+
+const Main = lazy(() => import('components/main/Main-idx'));
+const Error404 = lazy(() => import('components/error/404'));
+const Error500 = lazy(() => import('components/error/500'));
+const Inbx = lazy(() => import('components/inb/Inb-idx'));
+const AuthRoute = lazy(() => import('HOCs/AuthRoute'));
+const UnauthRoute = lazy(() => import('HOCs/UnauthRoute'));
+const UserLink = lazy(() => import('components/link/Link-idx'));
+const TemplateMSG = lazy(() => import('components/inb/template/Template'));
+const Msg = lazy(() => import('components/sendMsg/Msg-idx'));
+const Menu = lazy(() => import('components/partials/menu/Menu'));
+const Terms = lazy(() => import('components/terms/Terms'));
+const VerifyAccount = lazy(() => import('components/verifyAccount/VerifyAccount'))
 
 const loader = document.querySelector('#_l');
 
@@ -57,62 +57,66 @@ export default function App() {
 const RqqtComp = () => {
 	return (
 		<Router>
-			<Route path="/">
-				<Menu />
-			</Route>
-			<Route
-				render={({ location }) => (
-					<TransitionGroup component={null}>
-						<CSSTransition
-							key={location.key}
-							appear={true}
-                            timeout={{ enter: 250, exit: 0 }}
-                            classNames="fade"
-						>
-							<Switch location={location}>
-								<UnauthRoute
-									needsRenderTime={window.navigator.onLine}
-									exact
-									path="/"
-									component={Main}
-									redirectTo="/link"
-								/>
-								<AuthRoute
-									path="/messages"
-									component={Inbx}
-									redirectTo="/"
-									needsRenderTime={false}
-								/>
-								<AuthRoute
-									needsRenderTime={false}
-									path="/link"
-									component={UserLink}
-									redirectTo="/"
-								/>
-								<AuthRoute
-									needsRenderTime={false}
-									path="/message"
-									component={TemplateMSG}
-									redirectTo="/"
-								/>
-								<Route
-									path="/account/verify"
-									component={VerifyAccount}
-								/>
-								<Route path="/terms" component={Terms} />
-								<Route
-									exact
-									path="/:username"
-									component={Msg}
-								/>
-								<Route path="*">
-									<Error404 />
-								</Route>
-							</Switch>
-						</CSSTransition>
-					</TransitionGroup>
-				)}
-			/>
+			<Suspense fallback={<Loader/>}>
+				<Route path="/">
+					<Menu />
+				</Route>
+				<Route
+					render={({ location }) => (
+						<TransitionGroup component={null}>
+							<CSSTransition
+								key={location.key}
+								appear={true}
+								timeout={{ enter: 250, exit: 0 }}
+								classNames="fade"
+							>
+								<Switch location={location}>
+									<UnauthRoute
+										needsRenderTime={
+											window.navigator.onLine
+										}
+										exact
+										path="/"
+										component={Main}
+										redirectTo="/link"
+									/>
+									<AuthRoute
+										path="/messages"
+										component={Inbx}
+										redirectTo="/"
+										needsRenderTime={false}
+									/>
+									<AuthRoute
+										needsRenderTime={false}
+										path="/link"
+										component={UserLink}
+										redirectTo="/"
+									/>
+									<AuthRoute
+										needsRenderTime={false}
+										path="/message"
+										component={TemplateMSG}
+										redirectTo="/"
+									/>
+									<Route
+										path="/account/verify"
+										component={VerifyAccount}
+									/>
+									<Route path="/terms" component={Terms} />
+									<Route
+										exact
+										path="/:username"
+										component={Msg}
+									/>
+									<Route path="*">
+										<Error404 />
+									</Route>
+								</Switch>
+							</CSSTransition>
+						</TransitionGroup>
+					)}
+				/>
+			</Suspense>
 		</Router>
 	);
 };
