ec44d36a00460552989a20e7d3177d231fa4f3de
k0o <sb.knot@gmail.com>
2020-10-13 22:15:20 -0400

    refactor (client): main navigation flow

--- client/src/components/user-link/UserLink.tsx
+++ client/src/components/user-link/UserLink.tsx
@@ -4,11 +4,12 @@ import { useSelector, RootStateOrAny } from 'react-redux';
 import { InitContext } from '../../global/context/InitContext';
 import Slider from 'components/link/Slider';
 import Modal from 'components/link/Modal';
-import './Link-idx.css';
 import copy from 'assets/images/icons/icons-inbox/icon-link.svg';
 import tqIcon from 'assets/images/msg/profile-tq.png';
 
-export default function LinkIdx() {
+import './UserLink.css';
+
+export default function UserLink() {
 	const inputLink = useRef<HTMLInputElement>(null);
 	const { user } = useSelector((store: RootStateOrAny) => store.auth);
 	const [name, setName] = useState('');
@@ -20,8 +21,8 @@ export default function LinkIdx() {
 
 	const {
 		state: {
-			lang: { Link: lang },
-		},
+			lang: { Link: lang }
+		}
 	} = useContext(InitContext);
 
 	useEffect(() => {
@@ -74,7 +75,7 @@ export default function LinkIdx() {
 							styleName="btn-copied-link-help"
 							onClick={() => setShowModal(true)}
 							style={{
-								visibility: !showModal ? 'visible' : 'hidden',
+								visibility: !showModal ? 'visible' : 'hidden'
 							}}
 						>
 							?
