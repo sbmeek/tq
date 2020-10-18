25703efb3f557a831f5e941b5b7d3c60a64a3c67
k0o <sb.knot@gmail.com>
2020-10-17 23:51:38 -0400

    refactor (user-link): styled components

---
+++ client/src/components/user-link/UserLink.style.tsx
@@ -0,0 +1,138 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 100%;
+	padding-bottom: 10px;
+	min-height: 100%;
+`;
+
+export const InnerContainer = styled.div`
+	margin-top: 75px;
+`;
+
+export const Column = styled.div<{ margin?: string }>`
+	position: relative;
+	display: flex;
+	justify-content: center;
+	padding: 0 !important;
+	margin: ${(props) => props.margin};
+	& > div {
+		position: relative;
+	}
+	& > a {
+		text-decoration: none;
+	}
+`;
+
+export const UserLinkInput = styled.input`
+	position: absolute;
+	left: -999em;
+`;
+
+export const BtnHelp = styled.button<{ isVisible: boolean }>`
+	position: absolute;
+	top: 0;
+	left: 99.5%;
+	border-radius: 30px;
+	min-width: 20px;
+	min-height: 20px;
+	font-weight: 800;
+	font-size: 14px;
+	background: rgba(0, 0, 0, 0.2);
+	color: #fff;
+	cursor: pointer;
+	transition: background 180ms;
+	visibility: ${(props) => (props.isVisible ? 'visible' : 'hidden')};
+	&:hover {
+		background: rgba(0, 0, 0, 0.5);
+	}
+`;
+
+export const BtnTQ = styled.button`
+	display: inline-flex;
+	align-items: center;
+	margin-top: 3px;
+	font-family: 'Nunito', sans-serif;
+	box-shadow: 0px 4.5px rgb(88, 0, 3);
+	border-radius: 1000px;
+	background-color: #9e0004;
+	color: #fff;
+	min-width: 115px;
+	min-height: 55px;
+	border: 0;
+	font-size: 22.5px;
+	transition: ease-in-out 0.22s;
+	padding: 13px 15px;
+	overflow: hidden;
+	position: relative;
+	justify-content: center;
+	& > span {
+		display: block;
+		transition: transform 150ms ease-out;
+	}
+
+	&:hover > span {
+		transform: translateX(120px);
+	}
+
+	&:hover {
+		background-color: var(--tq-red-00);
+		color: rgb(88, 0, 3);
+		transition: ease-in 0.22s;
+		cursor: pointer;
+	}
+
+	& > img {
+		position: absolute;
+		transition: transform 150ms ease-out, opacity 200ms;
+		opacity: 0;
+		width: 22px;
+	}
+
+	&:hover > img {
+		transform: translateX(0);
+		opacity: 1;
+	}
+`;
+
+export const BtnCopyLink = styled.button`
+	font-family: 'Nunito', sans-serif;
+	display: flex;
+	text-transform: none;
+	box-shadow: 0px 6.4px var(--tq-blue-00);
+	border-radius: 30px;
+	background: var(--tq-blue-02);
+	min-width: 250px;
+	min-height: 50px;
+	border: 5.9px solid var(--tq-blue-01);
+	font-size: 22.5px;
+	transition: ease-in-out 0.22s;
+	margin-bottom: 10px;
+	color: var(--tq-blue-00);
+	padding: 6px 23px;
+	cursor: pointer;
+	overflow: hidden;
+	position: relative;
+	justify-content: center;
+	&:hover {
+		border: 5.9px solid var(--tq-blue-02);
+		background: var(--tq-blue-03);
+	}
+	& > img {
+		position: absolute;
+		transition: transform 150ms ease-out, opacity 200ms;
+		opacity: 0;
+		width: 33px;
+	}
+	&:hover > img {
+		transform: translateX(0);
+		opacity: 1;
+	}
+	& > span {
+		display: block;
+		transition: transform 150ms ease-out;
+	}
+	&:hover > span {
+		transform: translateX(220px);
+	}
+`;
--- client/src/components/user-link/UserLink.tsx
+++ client/src/components/user-link/UserLink.tsx
@@ -6,8 +6,15 @@ import Slider from 'components/link/Slider';
 import Modal from 'components/link/Modal';
 import copy from 'assets/images/icons/icons-inbox/icon-link.svg';
 import tqIcon from 'assets/images/msg/profile-tq.png';
-
-import './UserLink.css';
+import {
+	Container,
+	InnerContainer,
+	Column,
+	UserLinkInput,
+	BtnHelp,
+	BtnTQ,
+	BtnCopyLink
+} from './UserLink.style';
 
 export default function UserLink() {
 	const inputLink = useRef<HTMLInputElement>(null);
@@ -49,19 +56,12 @@ export default function UserLink() {
 	};
 
 	return (
-		<div styleName="container">
-			<div>
-				<Slider setShowPlaceholder={setShowPlaceholder} />
-			</div>
-			<div>
-				<div styleName="col">
-					<div styleName="col-inner-container">
-						<button
-							type="button"
-							styleName="btn-waves-effect-waves-light"
-							ref={btnLink}
-							onClick={copyLink}
-						>
+		<Container>
+			<Slider setShowPlaceholder={setShowPlaceholder} />
+			<InnerContainer>
+				<Column margin="0 0 15px 0">
+					<div>
+						<BtnCopyLink type="button" ref={btnLink} onClick={copyLink}>
 							{showCopiedLinkMsg ? (
 								<>{lang['BtnLinkCopiedToClipboard']}</>
 							) : (
@@ -70,40 +70,33 @@ export default function UserLink() {
 									<img src={copy} alt="copy" />
 								</>
 							)}
-						</button>
-						<button
-							styleName="btn-copied-link-help"
-							onClick={() => setShowModal(true)}
-							style={{
-								visibility: !showModal ? 'visible' : 'hidden'
-							}}
-						>
+						</BtnCopyLink>
+						<BtnHelp onClick={() => setShowModal(true)} isVisible={!showModal}>
 							?
-						</button>
+						</BtnHelp>
 						<Modal
 							modalTxt={`${lang['AlertLinkCopied']}.`}
 							showModal={showModal}
 							setShowModal={setShowModal}
 						/>
 					</div>
-					<input
+					<UserLinkInput
 						type="text"
 						value={link}
-						styleName="_tq-link-user"
 						aria-hidden="true"
 						ref={inputLink}
 						readOnly={true}
 					/>
-				</div>
-				<div styleName="col">
-					<Link to="/messages" styleName="a_btn-tq">
-						<button type="button" styleName="_btn-tq">
+				</Column>
+				<Column>
+					<Link to="/messages">
+						<BtnTQ type="button">
 							<span>{lang['BtnInbox']}</span>
 							<img src={tqIcon} alt="logo" />
-						</button>
+						</BtnTQ>
 					</Link>
-				</div>
-			</div>
-		</div>
+				</Column>
+			</InnerContainer>
+		</Container>
 	);
 }
