b63a155d576fb0253c8357c3f312dfd0604f9250
k0o <sb.knot@gmail.com>
2020-10-25 17:11:20 -0400

    fix (AuthInTestMode): timerID type

--- client/src/pages/authInTestMode/AuthInTestMode.tsx
+++ client/src/pages/authInTestMode/AuthInTestMode.tsx
@@ -1,60 +1,59 @@
-import React, { useState, useEffect, useContext } from 'react'
-import logo from 'assets/images/ltqrNEW.png'
-import { LoaderEye } from 'components/loader/Loader'
-import Axios from 'axios'
-import { InitContext, ActionEnum } from 'global/context/InitContext'
+import React, { useState, useEffect, useContext } from 'react';
+import logo from 'assets/images/ltqrNEW.png';
+import { LoaderEye } from 'components/loader/Loader';
+import Axios from 'axios';
+import { InitContext, ActionEnum } from 'global/context/InitContext';
 
-import styles from './AuthInTestMode.css'
+import styles from './AuthInTestMode.css';
 
-let timerID: NodeJS.Timeout
+let timerID: number;
 
 export const checkTst = async (val: string | 'tq_check_tst_init') => {
-    const res = await Axios.post('/user/tst_check', {
-        enteredKey: val,
-    })
-    return res.data.isTester;
-}
+	const res = await Axios.post('/user/tst_check', {
+		enteredKey: val
+	});
+	return res.data.isTester;
+};
 
 export default function ValdTester() {
-    const [isLoading, setIsLoading] = useState(false)
-    const { dispatch } = useContext(InitContext)
+	const [isLoading, setIsLoading] = useState(false);
+	const { dispatch } = useContext(InitContext);
 
-    useEffect(() => {
-        (async () => {
-            const isTester = await checkTst('tq_check_tst_init');
-            dispatch({
-                type: ActionEnum.SET_IS_TESTER,
-                payload: { isTester },
-            })
-        })();
-    }, [dispatch])
+	useEffect(() => {
+		(async () => {
+			const isTester = await checkTst('tq_check_tst_init');
+			dispatch({
+				type: ActionEnum.SET_IS_TESTER,
+				payload: { isTester }
+			});
+		})();
+	}, [dispatch]);
 
 	const handleKeyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const targetElement = e.target as HTMLInputElement
-		clearTimeout(timerID)
+		const targetElement = e.target as HTMLInputElement;
+		clearTimeout(timerID);
 		if (targetElement.value) {
 			timerID = setTimeout(() => {
-				setIsLoading(true)
+				setIsLoading(true);
 				setTimeout(async () => {
-                    const isTester = await checkTst(targetElement.value);
+					const isTester = await checkTst(targetElement.value);
 					if (!isTester) {
-                        targetElement.parentElement!.classList.add(styles['errored'])
-                        dispatch({
-                            type: ActionEnum.SET_IS_TESTER,
-                            payload: { isTester: false },
-                        })
-                    } 
-                    else {
-                        dispatch({
-                            type: ActionEnum.SET_IS_TESTER,
-                            payload: { isTester: true },
-                        })
+						targetElement.parentElement!.classList.add(styles['errored']);
+						dispatch({
+							type: ActionEnum.SET_IS_TESTER,
+							payload: { isTester: false }
+						});
+					} else {
+						dispatch({
+							type: ActionEnum.SET_IS_TESTER,
+							payload: { isTester: true }
+						});
 					}
-					setIsLoading(false)
-				}, 800)
-			}, 800)
+					setIsLoading(false);
+				}, 800);
+			}, 800);
 		}
-    }
+	};
 
 	return (
 		<div styleName="container">
@@ -78,5 +77,5 @@ export default function ValdTester() {
 				</div>
 			</form>
 		</div>
-	)
+	);
 }
