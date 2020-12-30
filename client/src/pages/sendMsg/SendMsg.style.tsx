5f8cdd1b5deff96eb53478fe52aff2eb1e054217
cqm2112 <61892716+cqm2112@users.noreply.github.com>
2020-12-30 12:40:04 -0400

    refactor SendMsg

---
+++ client/src/pages/sendMsg/SendMsg.style.tsx
@@ -0,0 +1,286 @@
+import styled, { keyframes } from 'styled-components';
+import layer from 'assets/images/msg/layer.png';
+
+export const Container = styled.div`
+	height: 100%;
+`;
+
+export const Curtain = styled.form`
+	min-height: 100%;
+	display: grid;
+	place-items: end;
+`;
+
+export const MiniCurtain = styled.div`
+	display: flex;
+	justify-content: center;
+	height: 80%;
+	min-width: 100vw;
+	background: #fff;
+`;
+
+export const CloudTop = styled.div`
+	display: flex;
+	justify-content: space-between;
+	position: fixed;
+	align-items: flex-end;
+	margin: -37px;
+	margin-right: 57px;
+
+	& > img {
+		width: auto;
+		height: 70px;
+		z-index: 1;
+		position: absolute;
+		bottom: 15px;
+		left: 125px;
+	}
+	& > img:nth-child(2) {
+		height: 58px;
+		right: 0px;
+		left: -119px;
+	}
+
+	@media only screen and (max-width: 542px) {
+		& > img:nth-child(1) {
+			height: 50px;
+		}
+		& > img:nth-child(2) {
+			height: 57px;
+		}
+	}
+`;
+
+export const Layer = styled.div`
+	background-image: url(${layer});
+	background-repeat: no-repeat;
+	background-size: 100% auto;
+	background-position: 50% 0%;
+	min-height: 80%;
+	width: 100%;
+	max-width: 880px;
+	margin-top: -62px;
+	padding-top: 66px;
+	max-height: 55%;
+
+	@media (max-width: 878px) {
+		background-size: 120% 62%;
+		min-height: 58%;
+		max-height: 650px;
+	}
+
+	@media (max-width: 600px) {
+		background-size: 164% 57%;
+		max-height: 570px;
+	}
+`;
+
+export const EnLayer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: -102px;
+
+	& > div:nth-child(1) {
+		box-sizing: content-box;
+		min-height: 157px;
+		min-width: 160px;
+		max-width: 170px;
+		border-radius: 100px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 5px solid #000000;
+		@media (max-width: 542px) {
+			max-width: 160px;
+		}
+	}
+	& > div:nth-child(1) > img {
+		width: 100%;
+		height: auto;
+	}
+	& > div:nth-child(2) {
+		background-color: #00000045;
+		width: 163px;
+		height: 9px;
+		margin: 15px;
+		border-radius: 50%;
+	}
+	& > h3 {
+		padding: 2px;
+		color: var(--tq-blue-05);
+		font-size: 264%;
+		font-family: 'Nunito', sans-serif;
+		margin-top: -13px;
+	}
+	& > h5 {
+		color: #efebeb;
+		font-size: 106%;
+		margin-top: -2px;
+	}
+	& > h5:nth-child(2) {
+		margin-top: -3px;
+		margin-bottom: 2px;
+	}
+`;
+
+export const Box = styled.div`
+	z-index: 1;
+	margin-top: 70px;
+
+	& > div:nth-child(1) {
+		display: flex;
+	}
+	& > div:nth-child(1) > button {
+		margin-left: 18px;
+		background: transparent;
+	}
+	& > div:nth-child(1) img {
+		height: 14px;
+		filter: invert(50%);
+	}
+	& > div:nth-child(1) > div {
+		display: flex;
+		justify-content: flex-end;
+		width: 58%;
+	}
+	& > div:nth-child(1) > div > button {
+		margin-left: 18px;
+		background: transparent;
+	}
+	& > div:nth-child(1) > div > img {
+		height: 21px;
+		width: 35px;
+		margin: -1px;
+		filter: invert(50%);
+	}
+
+	& > div:nth-child(2) {
+		position: fixed;
+		display: flex;
+		justify-content: center;
+		z-index: -1;
+		margin-top: 56px;
+	}
+	& > div:nth-child(2) > img {
+		width: 89px;
+		height: 70px;
+		transform: scaleX(-1);
+		margin-top: -68px;
+		margin-left: -27px;
+	}
+	& > div:nth-child(2) > img:nth-child(2) {
+		width: auto;
+		height: 68px;
+		margin-left: 138px;
+		margin-top: -69px;
+	}
+
+	@media (max-width: 480px) {
+		width: 282px;
+	}
+
+`;
+
+const ShowSubmitBtn = keyframes`
+0% {
+		transform: translateX(-2%);
+		opacity: 0;
+	}
+	1% {
+		display: grid;
+	}
+	100% {
+		transform: translateX(0);
+		opacity: 1;
+	}
+`;
+
+export const InputContainer = styled.div`
+	display: flex;
+	align-items: center;
+	position: relative;
+
+	&::after {
+		content: '';
+		display: block;
+		background-color: #0000004a;
+		width: 84%;
+		height: 8px;
+		border-radius: 50%;
+		bottom: -28px;
+		left: 15%;
+		z-index: -1;
+		position: absolute;
+	}
+
+	& > textarea {
+		width: 343px;
+		height: 135px !important;
+		resize: none;
+		font-family: 'Balsamiq Sans', cursive;
+		border-color: #969696 !important;
+		border: #969696 !important;
+		outline: none !important;
+		background-color: #d4d4d4;
+		border-radius: 12px;
+		font-size: 16px;
+		padding: 12px;
+		z-index: 1;
+		position: relative;
+	}
+
+	& > button {
+		font-family: 'Nunito', sans-serif;
+		text-transform: none;
+		background-color: var(--tq-blue-01);
+		width: 44px;
+		height: 50px;
+		border-radius: 10px 78px 78px 0px;
+		border: 0px solid #000000;
+		color: transparent;
+		display: none;
+		place-items: center;
+		position: absolute;
+		left: 100%;
+
+		&.active {
+			animation: ${ShowSubmitBtn} 200ms;
+			display: grid;
+		}
+
+		& > img {
+			max-height: 20px;
+		}
+	}
+`;
+
+export const CloudBottom = styled.div`
+	display: flex;
+	position: fixed;
+	justify-content: center;
+	margin-left: -99px;
+	margin-top: -26px;
+	z-index: 1;
+	& > img {
+		width: 180px;
+		height: auto;
+	}
+
+	@media only screen and (max-width: 542px) {
+		& > img:nth-child(2) {
+			width: 130px;
+			max-height: 300px;
+			margin-left: 30px;
+		}
+	}
+	@media only screen and (max-width: 480px) {
+		& > img:nth-child(1) {
+			height: 44px;
+		}
+		& > img:nth-child(2) {
+			width: auto;
+			height: 55px;
+		}
+	}
+`;
--- client/src/pages/sendMsg/SendMsg.tsx
+++ client/src/pages/sendMsg/SendMsg.tsx
@@ -3,11 +3,10 @@ import React, {
 	useState,
 	useContext,
 	ChangeEvent,
-	FormEvent,
+	FormEvent
 } from 'react';
 import MsgSent from 'components/msgSent/MsgSent';
 import Error404 from '../error404/Error404';
-import './SendMsg.css';
 import logo from 'assets/images/msg/profile-tq.png';
 import { InitContext } from 'global/context/InitContext';
 import cloud1 from 'assets/images/msg/cloud.png';
@@ -18,6 +17,17 @@ import Italic from 'assets/images/icons/templateEditor-icons/icons-message/icon-
 import strike from 'assets/images/icons/templateEditor-icons/icons-message/icon-strikethrough.svg';
 import underlined from 'assets/images/icons/templateEditor-icons/icons-message/icon-underlined.svg';
 import smile from 'assets/images/icons/templateEditor-icons/icons-message/iconfinder-Smile.svg';
+import {
+	Container,
+	Curtain,
+	MiniCurtain,
+	CloudTop,
+	Layer,
+	EnLayer,
+	Box,
+	InputContainer,
+	CloudBottom
+} from './SendMsg.style';
 
 type DataType = {
 	key?: string;
@@ -31,7 +41,7 @@ export default function <
 >({ match: { params } }: T) {
 	const {
 		socket,
-		lang: { MsgIdx: lang },
+		lang: { MsgIdx: lang }
 	} = useContext(InitContext).state;
 	const [userExists, setUserExists] = useState(true);
 	params.username = params.username.toLowerCase();
@@ -55,11 +65,7 @@ export default function <
 			{!userExists ? (
 				<Error404 />
 			) : (
-				<Success
-					username={params.username}
-					socket={socket}
-					lang={lang}
-				/>
+				<Success username={params.username} socket={socket} lang={lang} />
 			)}
 		</>
 	);
@@ -106,122 +112,56 @@ function Success<
 	}
 
 	return (
-		<div styleName="container">
+		<Container>
 			{sent ? (
 				<MsgSent />
 			) : (
-				<form onSubmit={handleFormSubmit} styleName="curtain">
-					<div styleName="contenedolsito">
-						<div styleName="cloud-top-container">
-							<img
-								src={cloud2}
-								alt="cloud2-top"
-								styleName="cloud2-top"
-							></img>
-							<img
-								src={cloud1}
-								alt="cloud-top"
-								styleName="cloud1-top"
-							></img>
-						</div>
-						<div styleName="layer">
-							<div styleName="en-layer">
-								<div styleName="logo-div">
-									<img
-										src={logo}
-										alt="logo"
-										styleName="logo"
-									/>
+				<Curtain onSubmit={handleFormSubmit}>
+					<MiniCurtain>
+						<CloudTop>
+							<img src={cloud2} alt="cloud2-top"></img>
+							<img src={cloud1} alt="cloud-top"></img>
+						</CloudTop>
+						<Layer>
+							<EnLayer>
+								<div>
+									<img src={logo} alt="logo" />
 								</div>
-								<div styleName="shadow_logo"></div>
-								<h3 styleName="user">{username}</h3>
-								<h5 styleName="firstRowText">
-									{lang['FirstRowText']}
-								</h5>
-								<h5 styleName="secondRowText">
-									{lang['SecondRowText']}
-								</h5>
-								<div styleName="box">
-									<div styleName="editor">
-										<button
-											title="Bold"
-											type="button"
-											styleName="buttonsedit"
-										>
-											<img
-												src={bold}
-												styleName="editorelement"
-												alt="editor"
-											/>
+								<div></div>
+								<h3>{username}</h3>
+								<h5>{lang['FirstRowText']}</h5>
+								<h5>{lang['SecondRowText']}</h5>
+								<Box>
+									<div>
+										<button title="Bold" type="button" >
+											<img src={bold} alt="editor" />
 										</button>
-										<button
-											title="Italic"
-											type="button"
-											styleName="buttonsedit"
-										>
-											<img
-												src={Italic}
-												styleName="editorelement"
-												alt="editor"
-											/>
+										<button title="Italic" type="button">
+											<img src={Italic} alt="editor" />
 										</button>
-										<button
-											title="strike"
-											type="button"
-											styleName="buttonsedit"
-										>
-											<img
-												src={strike}
-												styleName="editorelement"
-												alt="editor"
-											/>
+										<button title="strike" type="button">
+											<img src={strike} alt="editor" />
 										</button>
-										<button
-											title="Underline"
-											type="button"
-											styleName="buttonsedit"
-										>
-											<img
-												src={underlined}
-												styleName="editorelement"
-												alt="editor"
-											/>
+										<button title="Underline" type="button">
+											<img src={underlined} alt="editor" />
 										</button>
-										<div styleName="emoji">
-											<button
-												type="button"
-												styleName="buttonsedit"
-											>
-												<img
-													src={smile}
-													styleName="editoremoji"
-													alt="emojis"
-												/>
+										<div>
+											<button type="button" >
+												<img src={smile}  alt="emojis" />
 											</button>
 										</div>
 									</div>
-									<div styleName="cloud-bottom1-container">
-										<img
-											src={cloud2}
-											alt="cloud2-bottom"
-											styleName="cloud2-bottom"
-										></img>
-										<img
-											src={cloud1}
-											alt="cloud-bottom"
-											styleName="cloud1-bottom"
-										></img>
+									<div>
+										<img src={cloud2} alt="cloud2-bottom"></img>
+										<img src={cloud1} alt="cloud-bottom"></img>
 									</div>
-									<div styleName="input-container">
+									<InputContainer>
 										<textarea
-											styleName="input-msg"
 											data-type="text"
 											name="msg"
 											value={msg}
 											id="msg"
-											placeholder={
-												lang['FieldPlaceholder']
-											}
+											placeholder={lang['FieldPlaceholder']}
 											onChange={handleInputChange}
 											autoComplete="off"
 											autoFocus
@@ -229,26 +169,24 @@ function Success<
 										/>
 										<button
 											type="submit"
-											styleName={`_btn-tq ${
-												showSubmitBtn ? 'active' : ''
-											}`}
+											className={`${showSubmitBtn ? 'active' : ''}`}
 										>
 											<img src={arrow} alt="arrow icon" />
 										</button>
-									</div>
-									<div styleName="cloud-bottom2-container">
+									</InputContainer>
+									<CloudBottom >
 										<img
 											src={cloud1}
 											alt="cloud1-bottom2"
-											styleName="cloud1-bottom2"
+											
 										></img>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</form>
+									</CloudBottom>
+								</Box>
+							</EnLayer>
+						</Layer>
+					</MiniCurtain>
+				</Curtain>
 			)}
-		</div>
+		</Container>
 	);
 }
