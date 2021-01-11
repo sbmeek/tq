79463df05dd88af7462f3942fce245c782d379da
cqm2112 <61892716+cqm2112@users.noreply.github.com>
2021-01-11 17:31:40 -0400

    refactor(Root) Section4

--- client/src/components/root/Root.tsx
+++ client/src/components/root/Root.tsx
@@ -2,22 +2,13 @@ import React, { useContext, useState } from 'react';
 import InView from 'react-intersection-observer';
 import { InitContext } from 'global/context/InitContext';
 import AuthModal from '../authModal/AuthModal';
-import Interweave from 'interweave';
 import 'intersection-observer';
-
-import logoMaxSection4 from 'assets/images/presentation/logo_max_section4.png';
-import logoMinSection4 from 'assets/images/presentation/logo_min_section4.png';
-import logoReMinSection4 from 'assets/images/presentation/logo_remin_section4.png';
 import logoFooter from 'assets/images/presentation/logo_footer.svg';
-
-import bg4 from 'assets/images/presentation/bg4.svg';
-
 import arrowFooter from 'assets/images/presentation/arrow_lang-toggler.svg';
 import esFlag from 'assets/images/presentation/lang-flags/es-flag.svg';
 
 import {
 	Container,
-	Section,
 	HideMenucitoStyle,
 	Footer,
 	LogoFooterWrapper,
@@ -30,20 +21,10 @@ import {
 	InnerContainerFooter
 } from './Root.style';
 
-import {
-	Bg4,
-	FirstRowSection4,
-	LogoImgSection4,
-	LogoPicSection4,
-	ParagraphSection4,
-	SecondRowSection4,
-	TextWrapperSection4,
-	TitleSection4
-} from './Sections/Section4.style';
-
 import Section1 from './Sections/section1/Section1';
 import Section2 from './Sections/section2/Section2';
 import Section3 from './Sections/section3/Section3';
+import Section4 from './Sections/section4/Section4';
 import Section5 from './Sections/section5/Section5';
 
 export type SectionPropsType = {
@@ -90,30 +71,7 @@ export default function Root() {
 				</InView>
 				<InView skip={!isContainerLoaded} triggerOnce threshold={0.244}>
 					{({ inView, ref }) => (
-						<Section ref={ref} isVisible={inView}>
-							<FirstRowSection4>
-								<LogoPicSection4>
-									<source
-										media="(max-width: 390px)"
-										srcSet={logoReMinSection4}
-									/>
-									<source media="(max-width: 501px)" srcSet={logoMinSection4} />
-									<LogoImgSection4 src={logoMaxSection4} alt="logo-section4" />
-								</LogoPicSection4>
-							</FirstRowSection4>
-							<SecondRowSection4>
-								<Bg4 src={bg4} alt="bg4" />
-								<TextWrapperSection4>
-									<TitleSection4>{lang.Section4['Title']}</TitleSection4>
-									<ParagraphSection4>
-										<Interweave
-											allowAttributes
-											content={lang.Section4['Paragraph']}
-										/>
-									</ParagraphSection4>
-								</TextWrapperSection4>
-							</SecondRowSection4>
-						</Section>
+						<Section4 inView={inView} inViewRef={ref}></Section4>
 					)}
 				</InView>
 				<InView skip={!isContainerLoaded} triggerOnce threshold={0.244}>
